b6a5254c4da0aaba1e9e7521d34c053129993863
Nayeli <nayixpb0129@gmail.com>
2023-12-09 20:34:59 -0500

    cambios

---
+++ js/documento.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var miDocumento = document.getElementById("miDocumento");
+
+    // Incrustar el documento PDF utilizando el elemento <embed>
+    miDocumento.innerHTML = '<embed src="/documento/REVISTA-ARQUELOGÍA - PALEONTOLOGÍA-2015.pdf" type="application/pdf" width="800px" height="600px" />';
+});
