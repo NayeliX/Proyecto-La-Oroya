204ab8fadf61a1ce6a4985e771d65e2d73c44797
Nayeli <nayixpb0129@gmail.com>
2023-12-29 00:59:47 -0500

    cambios

--- js/direccionar.js
+++ js/direccionar.js
@@ -1,26 +1,26 @@
 function redirigir() {
-    window.location.href = "/html/recursos.html";
+    window.location.href = "html/recursos.html";
 }
 function redirigir0() {
-    window.location.href = "/html/vida-sociedad.html";
+    window.location.href = "html/vida-sociedad.html";
 }
 function redirigir1() {
-    window.location.href = "/html/vida_escolar.html";
+    window.location.href = "html/vida_escolar.html";
 }
 function redirigir2() {
-    window.location.href = "/html/trabajadores_empresas.html";
+    window.location.href = "html/trabajadores_empresas.html";
 }
 function redirigir3() {
-    window.location.href = "/html/luchas_sociales.html";
+    window.location.href = "html/luchas_sociales.html";
 }
 function redirigir4() {
-    window.location.href = "/html/hechos_historicos.html";
+    window.location.href = "html/hechos_historicos.html";
 }
 function redirigir5() {
-    window.location.href = "/html/feria.html";
+    window.location.href = "html/feria.html";
 }
 
 /* función retorno */
 function retornar() {
-    window.location.href = "/index.html";
+    window.location.href = "../index.html";
 }
--- js/documento.js
+++ js/documento.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
     var miDocumento = document.getElementById("miDocumento");
 
     // Incrustar el documento PDF utilizando el elemento <embed>
-    miDocumento.innerHTML = '<embed src="/documento/REVISTA-ARQUELOGÍA - PALEONTOLOGÍA-2015.pdf" type="application/pdf" width="800px" height="600px" />';
+    miDocumento.innerHTML = '<embed src="../documento/REVISTA-ARQUELOGÍA - PALEONTOLOGÍA-2015.pdf" type="application/pdf" width="800px" height="600px" />';
 });
